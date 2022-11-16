2e7c9aa8e281648083e5f6d82a8ed53d757a79b0
范勤阳 <fqymtu@gmail.com>
2022-11-17 00:26:08 +0800

    Add all tabs

--- src/components/ElementTabs.vue
+++ src/components/ElementTabs.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="element-tabs">
-    <el-tabs v-model="activeName" @tab-click="handleClick($event)" tab-position="left" type="card">
-      <el-tab-pane name="first">
-        <template #label>
-          <p>123</p>
-        </template>
-        <template #default>
-          <p>123213213</p>
-        </template>
-      </el-tab-pane>
-      <el-tab-pane label="tab-2" name="second">tab-2</el-tab-pane>
-      <el-tab-pane label="tab-3" name="third">tab-3</el-tab-pane>
-      <el-tab-pane label="tab-4" name="fourth">tab-4</el-tab-pane>
+    <el-tabs v-model="editableTabsValue" @tab-click="handleClick" tab-position="top" type="card" editable @edit="handleTabsEdit">
+      <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">{{ item.content }}</el-tab-pane>
     </el-tabs>
+    {{ tabIndex }}
   </div>
 </template>
 
@@ -21,12 +12,54 @@ export default {
   name: 'ElementTabs',
   data() {
     return {
-      activeName: 'third',
+      editableTabsValue: '2',
+      editableTabs: [
+        { title: 'Tab-1', name: '1', content: 'Tab-1-content' },
+        { title: 'Tab-2', name: '2', content: 'Tab-2-content' },
+        { title: 'Tab-3', name: '3', content: 'Tab-3-content' },
+      ],
     };
   },
+  computed: {
+    tabIndex() {
+      return this.editableTabs.length;
+    },
+  },
   methods: {
-    handleClick(e, name, tab) {
-      console.log(e, name, tab);
+    handleClick(targetName, action) {
+      console.log(targetName, action);
+    },
+    handleTabsEdit(targetName, action) {
+      if (action === 'add') {
+        let newTabName = String(++this.tabIndex);
+        this.editableTabs.push({ name: newTabName, title: `Tab-${++this.tabIndex}`, content: `Tab-${++this.tabIndex}-content` });
+        this.editableTabsValue = newTabName;
+      }
+      if (action === 'remove') {
+        // 如果删除的是选中的，设置删除之后，自动聚焦
+        let tabs = this.editableTabs;
+        let activeTab = this.editableTabsValue;
+        if (activeTab === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              console.log(tab);
+              console.log(targetName);
+              // 下一个要选中的tab
+              console.log(index);
+              let nextTab = tabs[index + 1] || tabs[index - 1];
+              console.log(nextTab); // tabs[-1]不存在，所以为 undefined
+              if (nextTab) {
+                this.editableTabsValue = nextTab.name;
+              } else {
+                console.log('删除的是最后一个');
+              }
+            }
+          });
+        }
+        this.editableTabs = this.editableTabs.filter((tab) => {
+          return tab.name !== targetName;
+        });
+      }
     },
   },
 };
